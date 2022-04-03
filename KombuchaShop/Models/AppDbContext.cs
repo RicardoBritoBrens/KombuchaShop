e8b91a935728e3ec097f74c6871e8f1a1eaea0ec
Ricardo Brito Brens <Ricardo_Brito@bancocaribe.com.do>
2022-04-03 08:40:28 -0400

    Add project files.

---
+++ KombuchaShop/Models/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KombuchaShop.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Kombucha> Kombuchas { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Kombucha>().HasData(new
+                List<Kombucha>()
+            {
+                new Kombucha()
+                {
+                }
+            });
+        }
+    }
+}
---
+++ KombuchaShop/Models/Kombucha.cs
@@ -0,0 +1,12 @@
+﻿namespace KombuchaShop.Models
+{
+    public class Kombucha
+    {
+        public int KombuchaId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string NutritiveInformation { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+    }
+}
